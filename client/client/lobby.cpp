7cdca0db73a25dad50a8e9cbb98945bed25134d0
LongPhanPip <phanlonghd5200@gmail.com>
2021-06-04 23:44:03 +0700

    Change constant

--- client/client/lobby.cpp
+++ client/client/lobby.cpp
@@ -4,9 +4,9 @@
 void create_lobby(Socket& socket, int team_number) {
 	char teamnum_str[TEAM_NUMBER_SIZE + 1];
 	_itoa_s(team_number, teamnum_str, TEAM_NUMBER_SIZE + 1, 10);
-	socket.tcp_send(CREATE_LOBBY, 0, teamnum_str);
+	socket.tcp_send(CREATE_GAME, 0, teamnum_str);
 }
 
 void join_lobby(Socket& socket, int id) {
-	socket.tcp_send(JOIN_LOBBY, id, "");
+	socket.tcp_send(CREATE_GAME, id, "");
 }
