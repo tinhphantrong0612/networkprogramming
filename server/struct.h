944d166ed16692756eccc1b17ea59b53aaddeb5a
Tình Phan <72553481+tinhphantrong0612@users.noreply.github.com>
2021-06-05 16:25:24 +0700

    Add a few constant and response function

--- server/struct.h
+++ server/struct.h
@@ -3,34 +3,12 @@
 typedef struct _wall {
 	int			type;
 	int			defense;
-	int			wood;
-	int			stone;
-	int			iron;
-
-	_wall(int _type, int _defense, int _wood, int _stone, int _iron) {
-		type	=	_type;
-		defense =	_defense;
-		wood	=	_wood;
-		stone	=	_stone;
-		iron	=	_iron;
-	}
 } *WALL;
 
 
 typedef struct _weapon {
 	int			type;
 	int			attack;
-	int			wood;
-	int			stone;
-	int			iron;
-
-	_weapon(int _type, int _attack, int _wood, int _stone, int _iron) {
-		type	=	_type;
-		attack	=	_attack;
-		wood	=	_wood;
-		stone	=	_stone;
-		iron	=	_iron;
-	}
 } *WEAPON;
 
 typedef struct _castle {
@@ -72,7 +50,6 @@ typedef struct _team {
 	int			index;
 	PLAYER		players[3];
 	int			castles[3];
-	int			mines[6];
 	WEAPON		weapon;
 	int			gold;
 	int			wood;
@@ -86,6 +63,7 @@ typedef struct _game {
 	long long	startAt;
 	int			gameState;
 	int			host;
+	int			teamNum;
 	PLAYER		players[PLAYER_NUM];
 	TEAM		teams[TEAM_NUM];
 	CASTLE		castles[CASTLE_NUM];
@@ -122,13 +100,30 @@ void updatePlayerInfo(PLAYER player, SOCKET socket, char *IP, int port, int team
 * @param	player			[IN/OUT]	Player
 * @return	nothing
 */
-void clearPlayerInfo(PLAYER player) {
+void clearPlayerInfo(PLAYER player, map<string, pair<string, int>> accountMap) {
 	// Remove player from game and team
 	if (player->game != NULL) {
 		GAME game = player->game;
-		game->players[player->gameIndex] = NULL;
-		game->teams[player->teamIndex]->players[player->placeInTeam] = NULL;
+		bool isLast = TRUE;
+		int first = 0;
+		for (int i = 0; i < PLAYER_NUM; i++) {
+			if (game->players[i] != NULL && player->gameIndex != i) {
+				isLast = FALSE;
+				first = i;
+				break;
+			}
+		}
+		if (isLast) {
+			emptyGame(game);
+		}
+		else {
+			game->players[player->gameIndex] = NULL;
+			if (player->gameIndex == game->host) game->host = first;
+			game->teams[player->teamIndex]->players[player->placeInTeam] = NULL;
+		}
 	}
+	map<string, pair<string, int>>::iterator it = accountMap.find(player->account);
+	it->second.second = NOT_AUTHORIZED;
 	updatePlayerInfo(player, 0, 0, 0, 0, 0, 0, 0, 0, NOT_AUTHORIZED);
 }
 
@@ -207,7 +202,7 @@ char *calculateGame(GAME game, char *buff) {
 int getCastleQuestion(CASTLE castle, char *fileName, char *buff) {
 	FILE *file;
 	fopen_s(&file, fileName, "r");
-	int seed = getTime() / 1000;
+	int seed = getTime() / 1000000;
 	srand(seed);
 
 	castle->question = rand() % HARDQUESTION_NUM; // randomize
@@ -226,7 +221,7 @@ int getCastleQuestion(CASTLE castle, char *fileName, char *buff) {
 int getMineWoodQuestion(MINE mine, char *fileName, char *buff) {
 	FILE *file;
 	fopen_s(&file, fileName, "r");
-	int seed = getTime() / 1000;
+	int seed = getTime() / 1000000;
 	srand(seed);
 
 	mine->woodQuestion = rand() % EASYQUESTION_NUM; // randomize
@@ -245,7 +240,7 @@ int getMineWoodQuestion(MINE mine, char *fileName, char *buff) {
 int getMineStoneQuestion(MINE mine, char *fileName, char *buff) {
 	FILE *file;
 	fopen_s(&file, fileName, "r");
-	int seed = getTime() / 1000;
+	int seed = getTime() / 1000000;
 	srand(seed);
 
 	mine->stoneQuestion = rand() % EASYQUESTION_NUM; // randomize
@@ -264,7 +259,7 @@ int getMineStoneQuestion(MINE mine, char *fileName, char *buff) {
 int getMineIronQuestion(MINE mine, char *fileName, char *buff) {
 	FILE *file;
 	fopen_s(&file, fileName, "r");
-	int seed = getTime() / 1000;
+	int seed = getTime() / 1000000;
 	srand(seed);
 
 	mine->ironQuestion = rand() % EASYQUESTION_NUM; // randomize
@@ -278,4 +273,91 @@ int getMineIronQuestion(MINE mine, char *fileName, char *buff) {
 	buff[0] = mine->ironQuestion / 256;
 	buff[1] = mine->ironQuestion % 256;
 	return questionLength;
+}
+
+/* The emptyGame function clear a game
+* @param	game			[IN/OUT]		Game
+* @return	nothing
+*/
+void emptyGame(GAME game) {
+	game->id = 0; // set id to 0
+	game->startAt = 0; // set start time to 0
+	game->gameState = WAITING; // set game state to waiting
+	game->host = 0;
+	for (int i = 0; i < PLAYER_NUM; i++) game->players[i] = NULL; // Disconnect to all player
+	for (int i = 0; i < CASTLE_NUM; i++) { // Unlink and clear castle
+		game->castles[i]->game = NULL;
+		free(game->castles[i]->wall);
+		game->castles[i]->wall = NULL;
+		free(game->castles[i]);
+		game->castles[i] = NULL;
+	}
+	for (int i = 0; i < MINE_NUM; i++) { // Unlink and clear mine
+		game->mines[i]->game = NULL;
+		free(game->mines[i]);
+		game->mines[i] = NULL;
+	}
+	for (int i = 0; i < TEAM_NUM; i++) { // Unlink and clear team
+		game->teams[i]->game = NULL;
+		free(game->teams[i]->weapon);
+		game->teams[i]->weapon = NULL;
+		for (int j = 0; j < 3; j++) {
+			game->teams[i]->castles[j] = 0;
+		}
+		for (int j = 0; j < 3; j++) {
+			game->teams[i]->players[j] = NULL;
+		}
+		free(game->teams);
+		game->teams[i] = NULL;
+	}
+}
+
+/* The createGame function create a game
+* @param	player			[IN/OUT]		Creator
+* @param	game			[IN/OUT]		Pointer to a emptyGame
+* @param	teamNum			[IN]			Number of teams in game
+* @return	nothing
+*/
+void createGame(PLAYER player, GAME game, int teamNum) {
+	game->id = getTime();
+	game->startAt = 0;
+	game->gameState = WAITING;
+	game->host = 0;
+	game->teamNum = teamNum;
+	game->players[0] = player;
+	for (int i = 0; i < CASTLE_NUM; i++) {
+		game->castles[i] = (CASTLE)malloc(sizeof(_castle));
+		game->castles[i]->game = game;
+		game->castles[i]->index = i;
+		game->castles[i]->wall = (WALL)malloc(sizeof(_wall));
+		game->castles[i]->wall->type = NO_WALL;
+		game->castles[i]->wall->defense = NO_WALL_DEF;
+		game->castles[i]->occupiedBy = -1;
+	}
+	for (int i = 0; i < MINE_NUM; i++) {
+		game->mines[i] = (MINE)malloc(sizeof(_mine));
+		game->mines[i]->game = game;
+		game->mines[i]->index = i;
+		game->mines[i]->iron = 0;
+		game->mines[i]->stone = 0;
+		game->mines[i]->wood = 0;
+	}
+	for (int i = 0; i < TEAM_NUM; i++) {
+		game->teams[i] = (TEAM)malloc(sizeof(_team));
+		game->teams[i]->game = game;
+		game->teams[i]->index = i;
+		for (int j = 0; j < 3; j++) {
+			game->teams[i]->castles[j] = 0;
+		}
+		for (int j = 0; j < 3; j++) {
+			game->teams[i]->players[j] = NULL;
+		}
+		game->teams[i]->weapon = (WEAPON)malloc(sizeof(_weapon));
+		game->teams[i]->gold = 0;
+		game->teams[i]->wood = 0;
+		game->teams[i]->stone = 0;
+		game->teams[i]->iron = 0;
+	}
+	game->teams[0]->players[0] = player;
+	updatePlayerInfo(player, player->socket, player->IP, player->port, 0, 0, 0, game, player->account, JOINT);
 }
--- server/utilities.h
+++ server/utilities.h
@@ -1,5 +1,5 @@
 #pragma once
-
+extern map<string, pair<string, int>> accountMap;
 /* The getTime function get the current time
 * @return	mili-seconds since 00:00:00 01/01/1970
 */
@@ -7,4 +7,24 @@ long long getTime() {
 	struct timespec timeSpec;
 	timespec_get(&timeSpec, TIME_UTC);
 	return 1000 * timeSpec.tv_sec + timeSpec.tv_nsec / 1000000;
+}
+
+void loadAccountMap(char *fileName) {
+	FILE *accountFile;
+	fopen_s(&accountFile, fileName, "r");
+	char buff[BUFF_SIZE];
+	char username[ACCOUNT_SIZE];
+	char password[ACCOUNT_SIZE];
+	while (fgets(buff, BUFF_SIZE, accountFile) != NULL) {
+		sscanf_s(buff, "%s %s", username, ACCOUNT_SIZE, password, ACCOUNT_SIZE);
+		accountMap[username] = make_pair(password, NOT_AUTHORIZED);
+	}
+	fclose(accountFile);
+}
+
+void addNewAccount(char *fileName, char *username, char *password) {
+	FILE *accountFile;
+	fopen_s(&accountFile, fileName, "w");
+	fprintf_s(accountFile, "%s %s\n", username, password);
+	fclose(accountFile);
 }
