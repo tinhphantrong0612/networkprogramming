1699918e9aaa20150d565f47cafd61cd6b074a65
LongPhanPip <phanlonghd5200@gmail.com>
2021-06-06 14:02:14 +0700

    Add change team func

--- client/client/util.h
+++ client/client/util.h
@@ -6,10 +6,13 @@ void pack(char* code, char* payload, char* mess);
 void auth_payload(char* username, char* password, char* mess);
 void join_lobby_payload(char* game_id, char* team_id, char* mess);
 
+// For login and signup
 Auth auth_data(char* payload);
 
+// Lobby activities
 Create_lobby create_lobby_data(char* payload);
 Get_lobby get_lobby_data(char* payload);
 Join_lobby join_lobby_data(char* payload);
 
+Change_team change_team_data(char* payload);
 void resolve_team_player_str(char* string, int team_number, int team_player[]);
